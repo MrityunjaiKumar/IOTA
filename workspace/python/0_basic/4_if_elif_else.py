062e583f5acccc99f27ab8bd0992d714c2b444fd
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-08 11:27:14 +0530

    elif added

---
+++ workspace/python/0_basic/4_if_elif_else.py
@@ -0,0 +1,16 @@
+var1="Hello World"
+
+if(len(var1)>5):
+    print("variable got more than 5 characters")
+elif var1=="Hello World":
+    print("Its hello world")
+else:
+    print("variable got less than 5 characters")
+
+
+if(len(var1)>15):
+    print("variable got more than 5 characters")
+elif var1=="Hello World":
+    print("Its hello world")
+else:
+    print("variable got less than 5 characters")
