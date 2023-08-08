3c00302d657047ac6cc507434d753caf8fb5cae9
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-08 11:52:44 +0530

    right side pyramid added

--- workspace/python/0_basic/6_pyramind.py
+++ workspace/python/0_basic/6_pyramind.py
@@ -19,3 +19,35 @@ for i in range(0, 5):
   # ending line after each row
   print("\r")
 
+
+
+print("-----------------------------")
+
+'''
+        * 
+      * * 
+    * * * 
+  * * * * 
+* * * * * 
+'''
+# number of spaces
+k = 2*5 - 2
+
+# outer loop to handle number of rows
+for i in range(0, 5):
+  
+    # inner loop to handle number spaces
+    for j in range(0, k):
+        print(end=" ")
+  
+    # decrementing k after each loop
+    k = k - 2
+  
+    # inner loop to handle number of columns
+    for j in range(0, i+1):
+      
+        # printing stars
+        print("* ", end="")
+  
+    # ending line after each row
+    print("\r")
