b0a9945bd7964e7f985ac79606709352f455dc09
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-08 14:02:09 +0530

    function added

---
+++ workspace/python/0_basic/7_function.py
@@ -0,0 +1,10 @@
+def isThisEvenNumber(x):
+    if x%2==0:
+        return True
+    return False
+
+print("2 ==> {}".format(isThisEvenNumber(2)))
+
+print("5 ==> {}".format(isThisEvenNumber(5)))
+
+print("10 ==> {}".format(isThisEvenNumber(10)))
