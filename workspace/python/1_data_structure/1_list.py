ec8b4546ed2379dc9e967ae642494f526e7b4a97
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-08 13:23:44 +0530

    data structure added

---
+++ workspace/python/1_data_structure/1_list.py
@@ -0,0 +1,25 @@
+# List declaration
+
+x=[6,1,2,8,3,4,5]
+
+print("x = {}".format(x))
+print(type(x))
+
+# Ascending order sort
+x.sort()
+print("x sorted = {}".format(x))
+
+#  Descending order sort
+x.sort(reverse=True)
+print("x reverse sorted = {}".format(x))
+
+#append
+x.append(12)
+print("x append = {}".format(x))
+
+#remove
+x.remove(2)
+print("x remove = {}".format(x))
+
+# getting an element, for example get 0th element
+print(x[0])
