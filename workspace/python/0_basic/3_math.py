dbadcf96a60e2891cd311a718748921daabaee7d
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-08 11:16:52 +0530

    basic math added

---
+++ workspace/python/0_basic/3_math.py
@@ -0,0 +1,26 @@
+# it should not be string
+
+x= "1"+"2"
+print(x)
+
+x1=1+2
+print(x1)
+
+x3="2"*3
+print(x3)
+
+
+x4=2**3
+print(x4)
+
+x5=2*3
+print(x5)
+
+x6=10/2
+print("x6 = {}".format(x6))
+
+x7=10//2
+print("x7 = {}".format(x7))
+
+x8=(10%2)
+print("x8 = {}".format(x8))
