1e931114ba058c410df7808a36395337b875bc30
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-08 14:10:47 +0530

    prime finction added

--- workspace/python/0_basic/7_function.py
+++ workspace/python/0_basic/7_function.py
@@ -8,3 +8,24 @@ print("2 ==> {}".format(isThisEvenNumber(2)))
 print("5 ==> {}".format(isThisEvenNumber(5)))
 
 print("10 ==> {}".format(isThisEvenNumber(10)))
+
+
+def isThisPrimeNum(x):
+    # If given number is greater than 1
+    if x > 1:
+        # Iterate from 2 to n / 2
+        for i in range(2, int(x/2)+1):
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (x % i) == 0:
+                return False
+                # break
+
+        else:
+            return True
+    else:
+        return False
+print("2 prime? = {}".format(isThisPrimeNum(2)))
+print("6 prime? = {}".format(isThisPrimeNum(6)))
+
+
