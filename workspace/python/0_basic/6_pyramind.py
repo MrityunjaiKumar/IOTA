ac6391e56db01dbd8c2e67ea9b728471ce5be9a0
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-08 11:47:06 +0530

    pyramind code added

---
+++ workspace/python/0_basic/6_pyramind.py
@@ -0,0 +1,22 @@
+''' Code 1
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+'''
+
+
+# outer loop to handle number of rows
+# n in this case
+for i in range(0, 5):
+
+  # inner loop to handle number of columns
+  for j in range(0, i+1):
+  
+    # printing stars
+    print("* ",end="")
+
+  # ending line after each row
+  print("\r")
+
