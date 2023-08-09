483bd1e525bbcc00a0724694c05f3e466c5b53ee
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-09 08:10:39 +0530

    equitriangle added

--- workspace/python/0_basic/6_pyramind.py
+++ workspace/python/0_basic/6_pyramind.py
@@ -51,3 +51,31 @@ for i in range(0, 5):
   
     # ending line after each row
     print("\r")
+
+# Function to demonstrate printing pattern triangle
+def eqi_triangle_star(n):
+     
+    # number of spaces
+    k = n - 1
+ 
+    # outer loop to handle number of rows
+    for i in range(0, n):
+     
+        # inner loop to handle number spaces
+        for j in range(0, k):
+            print(end=" ")
+     
+        # decrementing k after each loop
+        k = k - 1
+     
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+         
+            # printing stars
+            print("* ", end="")
+     
+        # ending line after each row
+        print("\r")
+ 
+eqi_triangle_star(5)
