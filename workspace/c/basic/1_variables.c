4fdaf4fdcd33a5d49725796d64f0bbeee6571af6
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-10 13:19:30 +0530

    variable added

--- workspace/c/basic/1_variables.c
+++ workspace/c/basic/1_variables.c
@@ -2,8 +2,18 @@
 
 // data_type variable_name=value;
 #include <stdio.h>
+#include <inttypes.h>
 
-void main() {
+void main()
+{
+  printf("sizeof char = %lu bytes\n", sizeof(char));
+  printf("sizeof int = %lu bytes\n", sizeof(int));
+  printf("sizeof long = %lu bytes\n", sizeof(long));
+  printf("sizeof long long = %lu bytes\n", sizeof(long long));
+
+  printf("sizeof int8_t = %lu bytes\n", sizeof(int8_t));
+
   int x = 1;
-  printf("x value = %d", x);
+  int8_t x2 = 1;
+  printf("x value = %d bytes\n", x);
 }
