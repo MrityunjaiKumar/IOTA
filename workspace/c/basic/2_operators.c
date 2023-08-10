95caa710cdbbec66f90ca66fa132aa286aa63fc6
hemantsharma64 <2021pietcshemant077@poornima.org>
2023-08-10 14:48:21 +0530

    bitwise complete

--- workspace/c/basic/2_operators.c
+++ workspace/c/basic/2_operators.c
@@ -14,9 +14,15 @@
 #define BIT(n) (1 << (n))
 
 int get_bit(int val, int _bit);
+int set_bit(int val, int _bit);
+int flip_bit(int val, int _bit);
+int clear_bit(int val, int _bit);
 void main()
 {
-    printf("%d", get_bit(6, 0));
+    printf("get_bit ==> %d\n", get_bit(6, 2));
+    printf("set_bit ==> %d\n", set_bit(6, 2));
+    printf("flip_bit ==> %d\n", flip_bit(6, 3));
+    printf("clear_bit ==> %d\n", clear_bit(6, 2));
 }
 /**
  * Get nth bit value
@@ -27,9 +33,26 @@ int get_bit(int val, int _bit)
     {
         return 1;
     }
-    else
-    {
-        return 0;
-    }
     return 0;
+}
+/**
+ * Set the nth bit
+ */
+int set_bit(int val, int _bit)
+{
+    return val | BIT(_bit);
+}
+/***
+ * flip BIt
+ */
+int flip_bit(int val, int _bit)
+{
+    val ^= BIT(_bit);
+    return val;
+}
+
+int clear_bit(int val, int _bit)
+{
+    val &= ~BIT(_bit);
+    return val;
 }
