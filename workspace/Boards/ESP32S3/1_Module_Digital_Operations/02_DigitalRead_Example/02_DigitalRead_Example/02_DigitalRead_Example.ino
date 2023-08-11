00adf4687c3e9aaee742dfda6dc6fbc3506e9cc9
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-11 12:05:45 +0530

    read command added

---
+++ workspace/Boards/ESP32S3/1_Module_Digital_Operations/02_DigitalRead_Example/02_DigitalRead_Example/02_DigitalRead_Example.ino
@@ -0,0 +1,35 @@
+
+/**
+ * @file 01_DigitalWrite_Example.ino
+ * @author Mrityunjai Kumar (mrityunjai@sincgrid.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-09-20
+ *
+ * Copyright Sincgrid LLP
+ *
+ * Problem statement:- Glow led when button is pressed, otherwise off
+ *
+ */
+
+#include <Arduino.h>
+
+#define LED_PIN 2
+#define BUTTON_PIN 4
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (!digitalRead(BUTTON_PIN)) {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
