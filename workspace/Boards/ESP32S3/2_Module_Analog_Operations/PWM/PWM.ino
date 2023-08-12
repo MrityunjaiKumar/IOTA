6257af04eee2e6dd30d9c29b829e2fdf8205897d
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2023-08-12 10:11:25 +0530

    pwm addeed

---
+++ workspace/Boards/ESP32S3/2_Module_Analog_Operations/PWM/PWM.ino
@@ -0,0 +1,32 @@
+// the number of the LED pin
+const int ledPin = 4;  
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannel = 2;
+const int resolution = 8;
+ 
+void setup(){
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPin, ledChannel);
+  
+}
+ 
+void loop(){
+  // increase the LED brightness
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);
+    delay(50);
+  }
+
+  // decrease the LED brightness
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(ledChannel, dutyCycle);   
+    delay(50);
+  }
+}
